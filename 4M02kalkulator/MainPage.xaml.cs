725fc155ba0b1feae33f471792567a774418f9ef
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-09-13 13:56:45 +0200

    Dodaj pliki projektów.

---
+++ 4M02kalkulator/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace _4M02kalkulator
+{
+    public partial class MainPage : ContentPage
+    {
+        int suma = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDodajClicked(object sender, EventArgs e)
+        {
+            int liczba = 0;
+            if (int.TryParse(entLiczba.Text, out liczba))
+                suma += liczba;
+            lblWynik.Text = $"suma: {suma.ToString()}";
+            entLiczba.Text = "";
+        }
+        private void btnRozmiarClick(object sender, EventArgs e)
+        {
+            int rozmiar = 0;
+            if(int.TryParse(enRozmiar.Text, out rozmiar))
+            {
+                lblNapis.FontSize = rozmiar;
+            }
+            enRozmiar.Text = "";
+        }
+    }
+
+}
